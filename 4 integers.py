21ed661279d227d2320bc6a6f231487fa7876343
Brandon Dickson <40203@longroad.ac.uk>
2014-09-12 16:04:09 +0100

    4 integers sum revision excersise

---
+++ 4 integers.py
@@ -0,0 +1,12 @@
+#Brandon Dickson
+#revision excersise
+#12-09-2014
+
+integer_1= int(input("please enter any number:"))
+integer_2= int(input("please enter any number:"))
+integer_3= int(input("please enter any number:"))
+integer_4= int(input("please enter any number:"))
+
+answer= integer_1 + integer_2 + integer_3 + integer_4
+
+print(answer)
