bf3e5d54d9a40eddef26bf4c7cf079dc7380c4cf
Brandon Dickson <40203@longroad.ac.uk>
2014-09-16 11:13:45 +0100

    revision excercise 3

---
+++ revision excercise 3.py
@@ -0,0 +1,13 @@
+#Brandon Dickson
+#Revision exercise 3
+#16-09-2014
+
+number_1=int(input("please enter any number:"))
+number_2=int(input("please enter any number:"))
+
+
+answer= number_1//number_2
+answer2=number_1%number_2
+
+print(answer)
+print(answer2)
