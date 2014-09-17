05c183ab66ec6c3d4667760b9d4665a4b1954206
Brandon Dickson <40203@longroad.ac.uk>
2014-09-17 14:56:51 +0100

    rvision exercise 3

---
+++ revision ex 3.py
@@ -0,0 +1,9 @@
+number_1=int(input("please enter any number:"))
+number_2=int(input("please enter any number:"))
+
+
+answer= number_1//number_2
+answer2=number_1%number_2
+
+print(answer)
+print("remainder:",answer2)
