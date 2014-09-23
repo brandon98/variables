06f477386a0208a35814f2d21a9182d2c45f29b2
brandon98 <40203@longroad.ac.uk>
2014-09-23 20:45:54 +0100

    strength and challenge 1

---
+++ strength and challenge exercise 1.py
@@ -0,0 +1,13 @@
+#Brandon Dickson
+#Strength and challenge excercise 1
+#23-09-2014
+
+length=int(input("Please enter the legth of the garden in cm:"))
+width=int(input("Please enter the width of the garden in cm:"))
+
+area=length*width
+
+cost=area*10
+
+
+print("Area of the garden(cm2):{0} Price for turf:£{1}".format(area,cost))
