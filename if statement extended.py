8b97379b2e7e7622b412752e192a37c5d3d9a626
brandon98 <40203@longroad.ac.uk>
2014-09-24 23:15:19 +0100

    IF statement exercise extended

---
+++ if statement extended.py
@@ -0,0 +1,12 @@
+#Brandon Dickson
+#If statement exercise extended
+#24-09-2014
+
+number=int(input("Please enter a number between 1 and 20:"))
+
+if number<=20 and number >=1:
+               print("In range.")
+elif number>20:
+               print("Number too high.")
+else:
+               print("Number too low.")
---
+++ if statement.py
@@ -0,0 +1,12 @@
+#Brandon Dickson
+#The 'IF' statement exercise
+#24-9-2014
+
+age=int(input("Please enter your age:"))
+if age>= 18:
+               print("You are old enough to vote.")
+else:
+                     print("You are too young to vote.")
+
+retirement=65-age
+print("Amount of years until you can retire:",retirement)
