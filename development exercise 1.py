21cb461d9c2bbf04857b05f208fb054d7adac04f
Brandon Dickson <40203@longroad.ac.uk>
2014-09-25 11:05:12 +0100

    development exercise 1

---
+++ development exercise 1.py
@@ -0,0 +1,11 @@
+#Brandon Dickson
+#Development exercise 1
+#25-09-2014
+
+number_1=int(input("Please enter any number:"))
+number_2=int(input("Please enter any number:"))
+
+answer= number_1 // number_2
+remainder= number_1 % number_2
+
+print("Answer:{0} Remainder:{1}".format(answer, remainder))
