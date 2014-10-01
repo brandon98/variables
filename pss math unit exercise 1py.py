5bca906366d313281c8133a2820c4949960357f7
brandon98 <40203@longroad.ac.uk>
2014-10-01 22:40:48 +0100

    PSS math unit exercise 1

---
+++ pss math unit exercise 1py.py
@@ -0,0 +1,13 @@
+#Brandon Dickson
+#PSS Math unit exercise 1
+#1-10-14
+
+import math
+
+radius=int(input("Please enter the radius:"))
+
+circumference= 2* math.pi*radius
+
+area= math.pi*radius**2
+
+print("circumference:{0} area:{1}".format(circumference, area))
