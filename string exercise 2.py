210e7566ff62b0d9dc05e3feb5dbe9aa8bcebd0e
brandon98 <40203@longroad.ac.uk>
2014-10-08 23:49:14 +0100

    string exercise 2

---
+++ string exercise 2.py
@@ -0,0 +1,14 @@
+#Brandon Dickson
+#String exercise 2
+#8-10-2014
+
+quote=input("Please enter quote:")
+replacement= input( "What word would you like to replace?")
+replacement2=input( "What word would you like to replace it with?")
+
+answer= quote.capitalize() 
+output= quote.replace(replacement, replacement2)
+
+
+print(output)
+
